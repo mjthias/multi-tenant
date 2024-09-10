d99a6485a3d68ebf011dfa4c5855e88dc350bc58
Mathias Dahl Jensen <mathias.j@forteadvice.com>
2024-09-10 17:04:53 +0200

    rm ;

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,8 @@
 export default function NotFound() {
-  <div>
-    <h1>Not Found</h1>
-    <p>Could not find requested resource</p>
-  </div>;
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p>Could not find requested resource</p>
+    </div>
+  );
 }
